afdf53bbb6a3945cb76ebb5ff7d65c1ceab6f1e9
Daniele Tolomelli <dtolomelli@develer.com>
2023-05-23 13:30:43 +0200

    Added drop to enemies

--- gameplay/scripts/Collectible.gd
+++ gameplay/scripts/Collectible.gd
@@ -8,6 +8,8 @@ class_name Collectible
 @onready var sprite_background = $Pivot/SpriteBackground
 @onready var player = $AnimationPlayer
 
+const BounceHeight = 20.0
+
 func init(item: Item):
 	self.item = item
 
@@ -15,6 +17,14 @@ func _ready():
 	self.item = item
 	sprite.texture = item.sprite
 	sprite_background.texture = item.sprite
+	
+	# play tween animation
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "position", position + BounceHeight * Vector2.UP, 0.25)\
+		.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CIRC)
+	tween.tween_property(self, "position", position, 0.5)\
+		.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BOUNCE)
+	tween.play()
 
 func _on_body_entered(body):
 	print("entered body")
